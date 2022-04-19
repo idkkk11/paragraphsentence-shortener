cf0f035e670bcdb381dfb1227b6c11bd13eefa83
idkkk11 <maw119@ic.ac.uk>
2022-04-19 20:35:58 +0100

    Added if statement to handle empty cells.

--- SentenceShortener.py
+++ SentenceShortener.py
@@ -5,9 +5,8 @@ import pandas as pd
 import numpy as np
 
 # Reading of excel file and importing the data as an array.
-# Reads from column C
 size = 0 
-df = pd.read_excel("spreadsheetfile.xlsx", index_col=None, usecols="C")
+df = pd.read_excel("DA_HongKong_QA.xlsx", index_col=None, usecols="C")
 array = df.to_numpy()
 flattened = array.flatten()
 size = flattened.size
@@ -22,16 +21,20 @@ list = ['Summarized Text']
 for i in range (0, size):
     text = flattened[i]
 
-    if len(text)>=250:      # Only summarises text with 250 characters or more, can be adjusted. (15 seconds)
-        print("Summarizing line ", i, "...")
-        summary_text = summarizer(text, max_length=100, min_length=5, do_sample=False)[0]['summary_text']
-        list.append(summary_text)
+    # To catch empty cells in the excel sheet.
+    if text == "" or text == np.nan or isinstance(text, str) == False:
+        list.append("")
     else:
-        print("Line ", i, " not summarised.")
-        list.append(text)
-
+        if len(text)>=250:      # Only summarises text with 250 characters or more, can be adjusted. (15 seconds)
+            print("Summarizing line ", i, "...")
+            summary_text = summarizer(text, max_length=100, min_length=5, do_sample=False)[0]['summary_text']
+            list.append(summary_text)
+        else:
+            print("Line ", i, " not summarised.")
+            list.append(text)
+  
 # Converts the list into a dataframe
 df2 = pd.DataFrame(list)
 
 # Converts and exports the dataframe into an excel file.
-df2.to_excel('./thearray4.xlsx')
+df2.to_excel('./HK.xlsx')
